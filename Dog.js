538b25ff9c07f0f90c6ee6ae26c70fd94b5c6b7b
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-06-01 10:36:54 -0500

    add logic to display profile-data

--- Dog.js
+++ Dog.js
@@ -1,1 +1,20 @@
 // Create the Dog class here
+class Dog {
+  constructor(data) {
+    Object.assign(this, data)
+  }
+  getProfileHTML() {
+    const {avatar, name, age, bio} = this
+    return `
+      <img src="${avatar}" alt="Photo of Teddy the dog" class="profile-img">
+      <img src="./images/badge-like.png" alt="Like badge" class="img-badge">
+      <img src="./images/badge-nope.png" alt="Nope badge" class="img-badge">
+      <div class="profile-text">
+          <h2>${name}, ${age}</h2>
+          <h3>${bio}</h3>
+      </div> 
+    `
+  }
+}
+
+export {Dog}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Remember to import the data and Dog class!
+import {Dog} from './Dog.js'
+import {dogs} from './data.js'
+
+const rex = new Dog(dogs[0])
+
+document.querySelector('.profile-container').innerHTML = rex.getProfileHTML()
